4381a397e3ced2ce0c9a5c70074349b307e194bf
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-10-08 17:17:31 -0600

    first commit

---
+++ archivo.py
@@ -0,0 +1,42 @@
+print('Bienvenido a Python')
+
+a = 2
+print(a)
+
+b = 5.4
+print(b)
+
+c = 'Clase de seguridad'
+print(c)
+
+#Este simbolo representa un comentario
+
+'''
+En esta seccion se crea un comentario multilinia
+print(c)
+'''
+
+#Operadores Aritmeticos
+d = a + b
+print(d)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+#Sintaxis de Condicionales
+edad = 16
+if edad > 18:
+    print('Mayor de edad')
+else:
+    print('Menor de edad')
+
+e, f, g = 3, 8, 9
+print((e + f)*g)
+
+imprimir = True
+
+if imprimir:
+    print('El valor de la operación es:', (e + f)*g)
+else:
+    print('No esta permitido imprimir')
