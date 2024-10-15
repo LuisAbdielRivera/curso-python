f4fd08a008028fe0ae5df189e38572d8bf38c6e4
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-10-15 16:50:01 -0600

    update

---
+++ archivo-3.py
@@ -0,0 +1,15 @@
+#Bucles: While, For, Breack, Comtinue
+
+#Ciclo while
+x = 0
+while x < 3:
+    print(x)
+    x+=1
+
+#Ciclo for
+print ("Ciclo for")
+for i in range(5):
+    print(i)
+
+#Ciclo for implementando salto
+for i in range(6)
