ec0af1981abdeada5b2f07e92673dacff9863670
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-10-22 09:29:01 -0600

    update

--- auto.py
+++ auto.py
@@ -1,37 +1,43 @@
 # Auto: Avanzar, Frenar, Apagar, Encender, GirarIzq, GirarDer
-def automovil():
-    estado = 'apagado'
-    def encender():
-        nonlocal estado
-        if estado == 'apagado':
-            print('Encendiendo')
-            estado = 'encendido'
-        else:
-            print('El automóvil está encendido')
-    def apagar():
-        nonlocal estado
-        if estado == 'encendido':
-            print('Apagando')
-            estado = 'apagado'
-        else:
-            print('El automóvil ya está apagado')
-    def avanzar():
-        if estado == 'encendido':
-            print('Avanzando')
-        else:
-            print('El automóvil debe estar encendido para avanzar')
-    def frenar():
-        if estado == 'encendido':
-            print('Frenando')
-        else:
-            print('El automóvil debe estar encendido para frenar')
-    def girar_izquierda():
-        if estado == 'encendido':
-            print('Girando a la izquierda')
-        else:
-            print('El automóvil debe estar encendido para frenar')
-    def girar_derecha():
-        if estado == 'encendido':
-            print('Frenando')
-        else:
-            print('Girando a la derecha')
+def Avanzar():
+    if llave:
+        print("Avanzando")
+    else:
+        print("Inserta la llave")
+
+def Encender():
+        global llave
+        llave = True
+
+def Apagar():
+        global llave
+        llave = False
+        print("Auto apagado")
+
+def GirarDer():
+        print("Girando a la Derecha")
+        global gDerecha
+        global gIzquierda
+        gDerecha = True
+        if gDerecha:
+                print("Ëstas girando a la derecha, no puedes girar a la Izquierda")
+                gIzquierda = False
+
+def GirarIzq():
+        print("Girando a la Izquierda")
+        global gDerecha
+        global gIzquierda
+        gIzquierda = True
+        if gIzquierda:
+                print("Ëstas girando a la izquierda, no puedes girar a la Derecha")
+                gDerecha = False
+
+def Frenar():
+        print("Parar Auto")
+
+Encender()
+Avanzar()
+GirarDer()
+GirarIzq()
+Frenar()
+Apagar()
